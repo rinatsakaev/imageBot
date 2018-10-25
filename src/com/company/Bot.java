a36f13401aa5cd287f9dc5e59963b62b30c76660
Pkononsky <43210059+Pkononsky@users.noreply.github.com>
2018-10-25 18:35:45 +0500

    Add files via upload

--- src/com/company/Bot.java
+++ src/com/company/Bot.java
@@ -1,4 +1,5 @@
 package com.company;
+
 import com.company.Helpers.GridFSUtil;
 import com.company.Helpers.OpenCVUtil;
 import com.company.Helpers.WebUtil;
@@ -6,8 +7,11 @@ import com.company.Models.Image;
 import com.company.Models.Profile;
 import com.company.Repos.ImageRepo;
 import com.company.Repos.ProfileRepo;
+
 import java.io.InputStream;
 import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Queue;
 import java.util.Scanner;
 
 public class Bot implements Runnable {
@@ -17,12 +21,15 @@ public class Bot implements Runnable {
     private WebUtil webUtil;
     private GridFSUtil gridFSUtil;
     private OpenCVUtil openCVUtil;
-    public Bot(String login){
+    private Queue<String> profileRequests;
+
+    public Bot(String login) {
         webUtil = new WebUtil();
         gridFSUtil = new GridFSUtil();
-        openCVUtil =  new OpenCVUtil();
+        openCVUtil = new OpenCVUtil();
         profileRepo = new ProfileRepo();
         imageRepo = new ImageRepo();
+        profileRequests = new PriorityQueue<>();
         authorize(login);
     }
 
@@ -30,38 +37,42 @@ public class Bot implements Runnable {
     public void run() {
         System.out.println("Команды: ls - показать загруженные картинки\n" +
                 "cb - обработать картинку");
-        String line = "";
-        Scanner sc = new Scanner(System.in);
         while (true) {
-            line = sc.nextLine();
-            try {
-                handleCommand(line);
-            } catch (Exception e){
-                break;
+            if (profileRequests.size() != 0) {
+                String message = profileRequests.poll();
+                try {
+                    handleCommand(message);
+                } catch (Exception e) {
+                    break;
+                }
             }
         }
     }
 
-    private void authorize(String login){
+    public void addToQueue(String message) {
+        profileRequests.add(message);
+    }
+
+    private void authorize(String login) {
         List<Profile> profiles = profileRepo.getAll();
-        for (Profile user:profiles) {
+        for (Profile user : profiles) {
             if (user.getLogin().equals(login))
                 profile = user;
         }
-        if (profile == null){
+        if (profile == null) {
             profile = new Profile(login);
             profileRepo.add(profile);
         }
     }
 
     private void handleCommand(String command) throws Exception {
-        switch (command){
+        switch (command) {
             case "ls":
-                if (profile.getImages() == null){
+                if (profile.getImages() == null) {
                     System.out.println("Нет картинок");
                     break;
                 }
-                for (Image img:profile.getImages())
+                for (Image img : profile.getImages())
                     System.out.println(img.getId());
                 break;
             case "cb":
@@ -71,19 +82,35 @@ public class Bot implements Runnable {
                 InputStream picture = webUtil.getStreamFromURL(url);
                 Image img = new Image(picture, profile);
                 imageRepo.add(img);
+
                 System.out.println("Круто. Теперь число от 1.0 до 3.0");
-                int alpha = sc.nextInt();
-                img.setBrightness(alpha);
+                img.setBrightness(waitNumber());
+
                 System.out.println("И еще одно от 0 до 100");
-                int beta = sc.nextInt();
-                img.setContrast(beta);
+                img.setContrast(waitNumber());
+
                 imageRepo.update(img);
                 gridFSUtil.getFileById(img.getId());
                 openCVUtil.ChangeBrightness(img.getId(), img.getBrightness(), img.getContrast());
                 System.out.println("Картинка готова");
                 break;
             case "exit":
-                    throw new Exception("Exit");
+                throw new Exception("Exit");
         }
+    }
+
+    private int waitNumber(){
+        int num = -1;
+        while (num == -1){
+            if (profileRequests.size() != 0)
+                try {
+                    num = Integer.parseInt(profileRequests.poll());
+                }
+                catch (Exception e){
+                    System.out.println("Это не то число >:C");
+                    num = -1;
+                }
+        }
+        return num;
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,17 +6,27 @@ public class Main {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         Bot bot = null;
+
+
         while (true){
             System.out.println("Введи логин");
             String login = sc.next();
             bot = new Bot(login);
             Thread botThread = new Thread(bot);
             botThread.start();
-            try {
-                botThread.join();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+            while (true){
+                String message = sc.nextLine();
+                //System.out.println("от лица профиля " + login);
+                if (message == "stop"){
+                    break;
+                }
+                bot.addToQueue(message);
             }
+            //try {
+            //    botThread.join();
+            //} catch (InterruptedException e) {
+            //    e.printStackTrace();
+            //}
         }
 
     }
