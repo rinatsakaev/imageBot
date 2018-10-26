cc7ddfabf1be48395b2074264ffe9757b639a41b
Pkononsky <43210059+Pkononsky@users.noreply.github.com>
2018-10-26 13:27:20 +0500

    Add files via upload

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,26 +1,36 @@
 package com.company;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         Bot bot = null;
-
+        Map<String, Bot> profileBot = new HashMap<>();
+        List<Thread> threads = new ArrayList<>();
 
         while (true){
             System.out.println("Введи логин");
-            String login = sc.next();
-            bot = new Bot(login);
-            Thread botThread = new Thread(bot);
-            botThread.start();
-            while (true){
-                String message = sc.nextLine();
-                if (message == "stop"){
-                    break;
-                }
-                bot.addToQueue(message);
+            String login = sc.nextLine();
+            if (!profileBot.containsKey(login)){
+                bot = new Bot(login);
+                Thread botThread = new Thread(bot);
+                botThread.start();
+                profileBot.put(login, bot);
+                threads.add(botThread);
             }
+
+            System.out.println("Введи сообщение");
+            String message = sc.nextLine();
+            if (message == "stop"){
+                break;
+            }
+            profileBot.get(login).addToQueue(message);
+
+        }
+
+        for (Thread thread : threads){
+            thread.stop();
         }
 
     }
